0cef8073cbb38d5ad94b849f951702e0f44bbe72
Hakan Sulun <3727398+alaminut@users.noreply.github.com>
2021-03-03 02:13:30 +0300

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '1fa6d374586150dadd00e3513632d3b9'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '4cad634a-d6d6-4ce9-86f3-d07c325175a9'
+};
